3f5f708424ba92526d3bfc2c6c491c96edb96a14
Max Sun <maxlouiesun@gmail.com>
2020-01-22 23:39:39 -0800

    Refactoring to use xz compression, and index data by URL rather than title. Also saving article title as well as article 'names'

--- make_readme.py
+++ make_readme.py
@@ -2,6 +2,7 @@
 import json
 import pandas as pd
 from datetime import datetime, timedelta
+import humanize
 
 def build_readme(page_data, path='Readme.md'):
 
@@ -13,17 +14,17 @@ def build_readme(page_data, path='Readme.md'):
         for link in p['links']:
             if link in page_data:
                 num_found += 1
-        return num_found / len(p['links'])
+        return (num_found / len(p['links']), num_found)
 
     num_scanned_pages = len(page_data)
     num_links = len(all_links)
 
-    titles_by_scan_ratio = sorted(
+    pages_by_scan_ratio = sorted(
         page_data.keys(),
         key=find_page_scan_ratio,
         reverse=True)
 
-    titles_by_doa = sorted(
+    pages_by_doa = sorted(
         page_data.keys(),
         key=lambda x: page_data[x]['doa'],
         reverse=True)
@@ -38,12 +39,12 @@ def build_readme(page_data, path='Readme.md'):
 
 This repo contains code for scanning Wikipedia and recording article summaries and links.
 
-`scan.py` scrapes summaries and saves the following data (indexed by article title):
+`scan.py` scrapes summaries and saves the following data (indexed by article url):
 
 ```
 {{
+    'title': str,
     'summary': str,
-    'url': str,
     'links': [str],
     'doa': datetime
 }}
@@ -51,12 +52,12 @@ This repo contains code for scanning Wikipedia and recording article summaries a
 
 `wiki.json.gz` contains the currently collected data.
 Most recently calculated stats:
-- Contains {0} summaries.
-- Contains {1} links.
-- {2}%% of the known graph has been scanned.
+- Contains {0:,} summaries.
+- Contains {1:,} links.
+- {2:.2f}% of the known graph has been scanned.
 - The most recently read article is "{3}".
-- The articles with the most scanned links are: {4}.
-- The average age of each scan is {5}.
+- The article with the most scanned links is "{4}".
+- On average, the scans were updated {5}.
 
 `python make_readme.py` will rebuild this Readme and update the stats.
 
@@ -65,16 +66,16 @@ Most recently calculated stats:
     """.format(
         num_scanned_pages,
         num_links,
-        num_scanned_pages / num_links,
+        100 * num_scanned_pages / num_links,
         titles_by_doa[0],
         titles_by_scan_ratio[0],
-        average_timedelta
+        humanize.naturaltime(average_timedelta)
         )
 
     open(path, 'w').write(readme)
 
 
 if __name__ == '__main__':
-    df = pd.read_json('./wiki.json.gz', compression='gzip', orient='columns', convert_dates=False)
+    df = pd.read_json('./wiki.json.xz', compression='xz', orient='columns', convert_dates=False)
     df['doa'] = df['doa'].apply(lambda x: datetime.utcfromtimestamp(x))
     build_readme(df.to_dict(orient='index'))
--- scan.py
+++ scan.py
@@ -10,18 +10,21 @@ from requests.exceptions import ConnectionError
 from wikipedia.exceptions import PageError, DisambiguationError
 
 def save(page_data):
+    start_time = datetime.now()
+    print('Saving - do not exist. This can take a few minutes.')
     df = pd.DataFrame.from_dict(page_data, orient='index')
     df['doa'] = df['doa'].apply(lambda x: x.timestamp())
     df.to_json(
-        path_or_buf='./wiki.json.gz',
-        compression='gzip',
+        path_or_buf='./wiki.json.xz',
+        compression='xz',
         )
     build_readme(page_data)
-    print('Saved', len(page_data), 'pages.')
+    duration = datetime.now() - start_time
+    print('Saved', len(page_data), 'pages in', duration)
 
 def load():
     try:
-        df = pd.read_json('./wiki.json.gz', compression='gzip', orient='columns', convert_dates=False)
+        df = pd.read_json('./wiki.json.xz', compression='xz', orient='columns', convert_dates=False)
         df['doa'] = df['doa'].apply(lambda x: datetime.utcfromtimestamp(x))
         return df.to_dict(orient='index')
     except FileNotFoundError:
@@ -31,8 +34,18 @@ def load():
 
 page_data = load()
 
-queue = ['Cognitive Science']
-MAX_ADDITIONS = 1000
+all_titles = set()
+all_links = set()
+for k in page_data.keys():
+    all_titles.update(page_data[k]['titles'])
+    all_links.update(page_data[k]['links'])
+
+print(len(all_titles))
+print(len(all_links))
+
+
+queue = ['United Nations']
+MAX_ADDITIONS = 10000
 num_additions = 0
 
 while num_additions < MAX_ADDITIONS:
@@ -41,21 +54,36 @@ while num_additions < MAX_ADDITIONS:
             # add a random existing title if the queue has nothing.
             queue.append(random.choice(list(page_data.keys())))
         
-        title = queue.pop(0)
+        title = queue.pop(0).replace('https://en.wikipedia.org/wiki/', '')
         links = []
         try:
             page = wikipedia.page(title)
-            page_data[title] = {
-                'summary': page.summary,
-                'links': page.links,
-                'url': page.url,
-                'doa': datetime.utcnow() #.strftime("%m/%d/%Y, %H:%M")
-            }
+            if page.url in page_data and title not in page_data[page.url]['titles']:
+                print('Adding {0} to {1}'.format(title, page_data[page.url]['titles']))
+                page_data[page.url]['titles'].append(title)
+                page_data[page.url]['doa'] = datetime.utcnow()
+                page_data[page.url]['summary'] = page.summary
+            else:
+                print('Read {0} to {1}'.format(title, page.title))
+                page_data[page.url] = {
+                    'name': page.title
+                    'titles': [title],
+                    'links': page.links,
+                    'doa': datetime.utcnow(),
+                    'summary': page.summary
+                }
+            all_titles.add(title)
+
+            # page_data[title] = {
+            #     'summary': page.summary,
+            #     'links': page.links,
+            #     'url': page.url,
+            #     'doa': datetime.utcnow() #.strftime("%m/%d/%Y, %H:%M")
+            # }
             links = page.links
-            print('Read:', page.title)
             num_additions += 1
 
-            if num_additions % 10 == 0:
+            if num_additions % 500 == 0:
                 print('Saving at checkpoint, after', num_additions, 'pages.')
                 save(page_data)
 
@@ -68,8 +96,8 @@ while num_additions < MAX_ADDITIONS:
             print('Failed to disambiguate:', title)
 
         for link in links:
-            if link not in page_data and link not in queue:
-                queue.append(link)
+            if link not in all_titles and link not in queue:
+                queue.append(link.replace('https://en.wikipedia.org/wiki/', ''))
 
     except KeyboardInterrupt as e:
         print('\nStopping after', num_additions, 'read pages.')
